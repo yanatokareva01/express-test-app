04b55eb229cac5f1b4614949396a81b36d7870ad
Yana Tokareva <yanatokareva01@gmail.com>
2017-07-03 22:08:32 +0300

    remove sorting from segment tree

--- src/utils.js
+++ src/utils.js
@@ -7,11 +7,6 @@ module.exports = {
 	SegmentTree: class SegmentTree {
 		constructor(array) {
 			this.array = array;
-			this.array.sort((a, b) => {
-				if (a < b) return -1;
-				if (a > b) return 1;
-				return 0;
-			});
 			this.tree = this.makeTree(array.length);
 			this.build(this.array, this.tree, 0, array.length - 1, 0);
 		}
@@ -24,17 +19,29 @@ module.exports = {
 				treeSize = n * 2 - 1;
 			} else {
 				treeSize = Math.pow(2, Math.ceil(exponent)) * 2 - 1;
+				let infinityCount = Math.ceil(treeSize /2) - this.array.length;
+				for (let i = 0; i < infinityCount; i++) {
+					this.array.push(Infinity);
+				}
 			}
 
 			let tree = Array(treeSize);
-			tree.fill(Infinity);
+			for (let i = 0; i < tree.length; i++) {
+				tree[i] = {
+					min: Infinity,
+					l: Infinity,
+					r: Infinity
+				};
+			}
 
 			return tree;
 		}
 
 		build(array, tree, l, r, i) {
 			if (l == r) {
-				tree[i] = array[l];
+				tree[i].l = array[l];
+				tree[i].r = array[l];
+				tree[i].min = array[l];
 				return;
 			}
 
@@ -43,26 +50,26 @@ module.exports = {
 			this.build(array, tree, l, mid, i * 2 + 1);
 			this.build(array, tree, mid + 1, r, i * 2 + 2);
 
-			tree[i] = Math.min(tree[i * 2 + 1], tree[i * 2 + 2]);
+			tree[i].l = Math.min(tree[i*2 + 1].l, tree[i*2 + 2].l);
+			tree[i].r = Math.max(tree[i*2 + 1].r, tree[i*2 + 2].r);
+			tree[i].min = Math.min(tree[i * 2 + 1].min, tree[i * 2 + 2].min);
 		}
 
 		rmq(l, r) {
-			return this.rangeMinQuery(this.tree, l, r, 0, this.array.length - 1, 0);
+			return this.rangeMinQuery(this.tree, l, r, 0);
 		}
 
-		rangeMinQuery(tree, ql, qr, l, r, i) {
-			if (ql <= this.array[l] && qr >= this.array[r]) {
-				return tree[i];
+		rangeMinQuery(tree, ql, qr, i) {
+			if (ql <= this.tree[i].l && qr >= this.tree[i].r) {
+				return tree[i].min;
 			}
-			if (this.array[l] > qr || this.array[r] < ql) {
+			if (this.tree[i].l > qr || this.tree[i].r < ql) {
 				return Infinity;
 			}
 
-			let mid = Math.floor((l + r) / 2);
-
 			return (Math.min(
-				this.rangeMinQuery(tree, ql, qr, l, mid, i * 2 + 1),
-				this.rangeMinQuery(tree, ql, qr, mid + 1, r, i * 2 + 2)
+				this.rangeMinQuery(tree, ql, qr, i * 2 + 1),
+				this.rangeMinQuery(tree, ql, qr, i * 2 + 2)
 			));
 		}
 	},
